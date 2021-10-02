918a93403ca4c0fd585442bf701729184878d7dc
Gabe Corridore <corridg@rose-hulman.edu>
2021-10-01 22:40:48 -0400

    Added company rankings

--- app/graphql/resolvers/Member.js
+++ app/graphql/resolvers/Member.js
@@ -2,6 +2,7 @@ import {gql} from "apollo-server-express";
 import {authenticateResolver} from "../../domain/auth/resolvers/authenticateResolver";
 import {CompanyMembersType, getAllMembers} from "./MemberQueries/GetAllMembers"
 import {MemberProgressType, getMemberProgress} from "./MemberQueries/GetMemberProgress"
+import { RankedMembersType, getMemberRankings } from "./MemberQueries/GetMemberRankings";
 import AppConfigs from "../../configs/app_configs"
 import { Op } from "sequelize";
 
@@ -24,9 +25,12 @@ export const typeDef = gql`
     type MemberProgress ${MemberProgressType}
 
     type CompanyMembers ${CompanyMembersType}
+
+    type RankedMember ${RankedMembersType}
     
     extend type Query {
         getAllMembers: CompanyMembers!
+        getMemberRankings: [RankedMember]!
         getAuthUserProgress: MemberProgress!
         getCurrentEmployees: [Member]!
     }
@@ -35,6 +39,7 @@ export const typeDef = gql`
 const MembersResolvers = {
     Query: {
         getAllMembers: (parent, args, {db}, info) => getAllMembers(parent, args, {db}),
+        getMemberRankings: authenticateResolver({app: [AppConfigs.permissions.OWNER,AppConfigs.permissions.MANAGER,AppConfigs.permissions.MEMBER]}, (parent, args, {db, user}, info) => getMemberRankings(db, user)),
         getCurrentEmployees: authenticateResolver({app: [AppConfigs.permissions.OWNER,AppConfigs.permissions.MANAGER]}, (parent, args, {db}, info) => db.members.findAll({where: {company: { [Op.ne]: 'fired' }}})),
         getAuthUserProgress: authenticateResolver({app: [AppConfigs.permissions.OWNER,AppConfigs.permissions.MANAGER,AppConfigs.permissions.MEMBER]}, (parent, args, {db, user}, info) => getMemberProgress(parent, args, {db, user}))
     }
