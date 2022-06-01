722c1c9925fd858016f3b2d40a306646220c1d5f
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2022-06-01 15:20:17 +0100

    Add quickCahce

--- app/tycoon-sdk/lib/apiRequest.js
+++ app/tycoon-sdk/lib/apiRequest.js
@@ -1,86 +1,124 @@
 // @flow
-import { fetchBuilder, MemoryCache } from 'node-fetch-cache';
 
-const zeroFetch = fetchBuilder.withCache(new MemoryCache({ttl: 50}));
-const shortFetch = fetchBuilder.withCache(new MemoryCache({ttl: 2 * 60000}));
-const longFetch = fetchBuilder.withCache(new MemoryCache({ttl: 10 * 60000}));
+import { MemoryCache, fetchBuilder } from "node-fetch-cache";
 
-const apiRequest = (request) => {
-	const {responseType} = request
+const zeroFetch = fetchBuilder.withCache(new MemoryCache({ ttl: 50 }));
+const quickFetch = fetchBuilder.withCache(new MemoryCache({ ttl: 5 * 1000 }));
+const shortFetch = fetchBuilder.withCache(new MemoryCache({ ttl: 2 * 60000 }));
+const longFetch = fetchBuilder.withCache(new MemoryCache({ ttl: 10 * 60000 }));
 
-	if (request.cache === 'OFF') {
+const apiRequest = request => {
+	const { responseType } = request;
+
+	if (request.cache === "OFF") {
 		return zeroFetch(request.url, {
 			method: request.method,
 			headers: request.headers,
 			body: request.body,
 			timeout: request.timeout
-		})
-		.then((response) => {
-			if(response.ok) {
-				if (responseType == 'TYCOON') {
-					return response.text().then((html) => {
-						return JSON.parse(new DOMParser().parseFromString(html, "text/html").body.innerHTML.replace(/<\/?[^>]+>/gi, ''))
-					})
-				} else if (responseType == 'EMPTY') {
+		}).then(response => {
+			if (response.ok) {
+				if (responseType == "TYCOON") {
+					return response.text().then(html => {
+						return JSON.parse(
+							new DOMParser()
+								.parseFromString(html, "text/html")
+								.body.innerHTML.replace(/<\/?[^>]+>/gi, "")
+						);
+					});
+				} else if (responseType == "EMPTY") {
 					return true;
 				} else {
-					return response[responseType]()
+					return response[responseType]();
 				}
 			} else {
-				throw response.text().then((text) => {
+				throw response.text().then(text => {
 					return text;
-				})
+				});
 			}
-		})
-	} else if (request.cache === 'SHORT') {
+		});
+	} else if (request.cache === "QUICK") {
+		return quickFetch(request.url, {
+			method: request.method,
+			headers: request.headers,
+			body: request.body,
+			timeout: request.timeout
+		}).then(response => {
+			if (response.ok) {
+				if (responseType == "TYCOON") {
+					return response.text().then(html => {
+						return JSON.parse(
+							new DOMParser()
+								.parseFromString(html, "text/html")
+								.body.innerHTML.replace(/<\/?[^>]+>/gi, "")
+						);
+					});
+				} else if (responseType == "EMPTY") {
+					return true;
+				} else {
+					return response[responseType]();
+				}
+			} else {
+				throw response.text().then(text => {
+					return text;
+				});
+			}
+		});
+	} else if (request.cache === "SHORT") {
 		return shortFetch(request.url, {
 			method: request.method,
 			headers: request.headers,
 			body: request.body,
 			timeout: request.timeout
-		})
-		.then((response) => {
-			if(response.ok) {
-				if (responseType == 'TYCOON') {
-					return response.text().then((html) => {
-						return JSON.parse(new DOMParser().parseFromString(html, "text/html").body.innerHTML.replace(/<\/?[^>]+>/gi, ''))
-					})
-				} else if (responseType == 'EMPTY') {
+		}).then(response => {
+			if (response.ok) {
+				if (responseType == "TYCOON") {
+					return response.text().then(html => {
+						return JSON.parse(
+							new DOMParser()
+								.parseFromString(html, "text/html")
+								.body.innerHTML.replace(/<\/?[^>]+>/gi, "")
+						);
+					});
+				} else if (responseType == "EMPTY") {
 					return true;
 				} else {
-					return response[responseType]()
+					return response[responseType]();
 				}
 			} else {
-				throw response.text().then((text) => {
+				throw response.text().then(text => {
 					return text;
-				})
+				});
 			}
-		})
-	} else if (request.cache === 'LONG') {
+		});
+	} else if (request.cache === "LONG") {
 		return longFetch(request.url, {
 			method: request.method,
 			headers: request.headers,
 			body: request.body,
 			timeout: request.timeout
-		})
-		.then((response) => {
-			if(response.ok) {
-				if (responseType == 'TYCOON') {
-					return response.text().then((html) => {
-						return JSON.parse(new DOMParser().parseFromString(html, "text/html").body.innerHTML.replace(/<\/?[^>]+>/gi, ''))
-					})
-				} else if (responseType == 'EMPTY') {
+		}).then(response => {
+			if (response.ok) {
+				if (responseType == "TYCOON") {
+					return response.text().then(html => {
+						return JSON.parse(
+							new DOMParser()
+								.parseFromString(html, "text/html")
+								.body.innerHTML.replace(/<\/?[^>]+>/gi, "")
+						);
+					});
+				} else if (responseType == "EMPTY") {
 					return true;
 				} else {
-					return response[responseType]()
+					return response[responseType]();
 				}
 			} else {
-				throw response.text().then((text) => {
+				throw response.text().then(text => {
 					return text;
-				})
+				});
 			}
-		})
+		});
 	}
-}
+};
 
-export default apiRequest
+export default apiRequest;
